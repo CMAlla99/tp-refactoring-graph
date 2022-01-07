46b60de61cab45c833aecee3dca5dc1136b2da6c
CMAlla <clairemarie.alla@gmail.com>
2022-01-07 12:35:02 +0100

    Question 0.4 - ajout de tests cost et geom

--- src/test/java/org/acme/graph/model/EdgeTest.java
+++ src/test/java/org/acme/graph/model/EdgeTest.java
@@ -3,8 +3,11 @@ package org.acme.graph.model;
 import org.junit.Assert;
 import org.junit.Test;
 import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.LineString;
 
 public class EdgeTest {
+
+	public static final double EPSILON = 1.0e-15;
 
     @Test
 	public void testConstructor(){
@@ -14,6 +17,22 @@ public class EdgeTest {
 		Edge e = new Edge(v1, v2);
 		Assert.assertEquals(v1, e.getSource());
 		Assert.assertEquals(v2, e.getTarget());
+	}
+
+	@Test
+	public void testgetCost() {
+		Vertex v1 = new Vertex(new Coordinate(1.0, 1.0), "1");
+        Vertex v2 = new Vertex(new Coordinate(3.0, 1.0), "2");
+		Edge e = new Edge(v1, v2);
+		Assert.assertEquals(0.0, e.getCost(), EPSILON);
+	}
+
+	@Test
+	public void testgetGeometry() {
+		Vertex v1 = new Vertex(new Coordinate(1.0, 1.0), "1");
+        Vertex v2 = new Vertex(new Coordinate(3.0, 1.0), "2");
+		Edge e = new Edge(v1, v2);
+		Assert.assertTrue(e.getGeometry() != null);
 	}
     
 }
